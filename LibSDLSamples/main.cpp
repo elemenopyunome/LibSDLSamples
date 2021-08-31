41ad7f6bd3533f08e9ae44c2d5b12702dab4fe25
Ele Menopy <elemenopyunome@gmail.com>
2021-08-31 00:01:53 -0500

    This app needs to be able to move between MVT files with arrow keys

--- LibSDLSamples/main.cpp
+++ LibSDLSamples/main.cpp
@@ -6,81 +6,120 @@
 #include "Downloader.h"
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1024
-
+double round(double x, int n) {
+    int d = 0;
+    if ((x * pow(10, n + 1)) - (floor(x * pow(10, n))) > 4) d = 1;
+    x = (floor(x * pow(10, n)) + d) / pow(10, n);
+    return x;
+}
 int main(int argc, char* args[]) {
-    
+    double LatValue = -97.01222;
+    double LongValue = 33.04583;
     
     TileVectorHelper _tvhelper;
-    std::string FileURL = _tvhelper.GetVectorLocationByLatAndLong(-95.42842, 29.05344);
+    std::string FileURL = _tvhelper.GetVectorLocationByLatAndLong(LatValue, LongValue);
 
     HTTPDownloader _dl;
-    //std::string Info = _dl.downloadtostring(FileURL);
     std::string FilePath = _dl.download_mvt(FileURL);
     FileHelper _helper;
     std::vector<std::string> info = _helper.GetVectors(FilePath);
     int vectsize = info.size();
-    /*for (int i = 0; i < vectsize; i++)
-    {
-        std::string row = info[i];
-        std::vector<std::string> entirevector = _helper.split(row, ',');
-    }*/
     int MajorDivider = 3;
-    std::string bla = "";
-
     if (SDL_Init(SDL_INIT_VIDEO) == 0) {
         SDL_Window* window = NULL;
         SDL_Renderer* renderer = NULL;
-        
+        SDL_Event event;
+        int quit = 0;
+        double Multiplier = 0.02;
         if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN, &window, &renderer) == 0) {
             SDL_bool done = SDL_FALSE;
-
-            while (!done) {
-                SDL_Event event;
-
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                SDL_RenderClear(renderer);
-
-                SDL_SetRenderDrawColor(renderer, 255, 255, 51, SDL_ALPHA_OPAQUE);
-                for (int i = 0; i < vectsize; i++)
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            while (!quit)
+            {
+                while (SDL_PollEvent(&event))
                 {
-                    std::string row = info[i];
-                    std::vector<std::string> entirevector = _helper.split(row, ',');
-                    int x1 = std::stoi(entirevector[0]) / MajorDivider;
-                    int y1 = std::stoi(entirevector[1]) / MajorDivider;
-                    int x2 = std::stoi(entirevector[2]) / MajorDivider;
-                    int y2 = std::stoi(entirevector[3]) / MajorDivider;
-                    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-                    //go crazy
-                    //SDL_RenderDrawLine(renderer, y1, x1, y1, x2);
-                    SDL_RenderPresent(renderer);
-                    //SDL_Delay(50);
-                }
-                /*for (int i = 0; i < 500; i++)
-                {
-                    SDL_SetRenderDrawColor(renderer, 94, 155, 159, SDL_ALPHA_OPAQUE);
-                    SDL_RenderDrawLine(renderer, 330+i, 260+i, 129, 359 + i);
-                    SDL_SetRenderDrawColor(renderer, 233, 155, 66, SDL_ALPHA_OPAQUE);
-                    SDL_RenderDrawLine(renderer, 230 + i, 359 + i, 228, 258 + i);
-                    SDL_SetRenderDrawColor(renderer, 156, 78, 45, SDL_ALPHA_OPAQUE);
-                    SDL_RenderDrawLine(renderer, 130 + i, 160 + i, 329, 159 + i);
-                    SDL_SetRenderDrawColor(renderer, 75, 155, 88, SDL_ALPHA_OPAQUE);
-                    SDL_RenderDrawLine(renderer, 230 + i, 360 + i, 329, 259 + i);
-                    SDL_SetRenderDrawColor(renderer, 255, 255, 11, SDL_ALPHA_OPAQUE);
-                    SDL_RenderDrawLine(renderer, 330 + i, 159 + i, 128, 358 + i);
-                    SDL_SetRenderDrawColor(renderer, 76, 34, 65, SDL_ALPHA_OPAQUE);
-                    SDL_RenderDrawLine(renderer, 130 + i, 260 + i, 229, 159 + i);
-                    SDL_RenderPresent(renderer);
-                    SDL_Delay(100);
-                }*/
-                SDL_Delay(19000);
-                break;
-
-                while (SDL_PollEvent(&event)) {
-                    if (event.type == SDL_QUIT) {
-                        done = SDL_TRUE;
+                    switch (event.type)
+                    {
+                        case SDL_KEYDOWN:
+                            switch (event.key.keysym.sym)
+                            {
+                            case SDLK_LEFT:
+                                LongValue += Multiplier;
+                                break;
+                            case SDLK_RIGHT:
+                                LongValue -= Multiplier;
+                                break;
+                            case SDLK_UP:
+                                LatValue += Multiplier;
+                                break;
+                            case SDLK_DOWN:
+                                LatValue -= Multiplier;
+                                break;
+                            case SDLK_x:
+                                quit = 1;
+                                break;
+                            default:
+                                break;
+                            }
                     }
+                    FileURL = _tvhelper.GetVectorLocationByLatAndLong(round(LatValue,6), round(LongValue,6));
+
+                    FilePath = _dl.download_mvt(FileURL);
+                    std::vector<std::string> infotemp = _helper.GetVectors(FilePath);
+                    int vectsize = infotemp.size();
+                    SDL_RenderClear(renderer);
+                    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+                    SDL_RenderClear(renderer);
+                    SDL_RenderPresent(renderer);
+                    
+                    SDL_SetRenderDrawColor(renderer, 255, 255, 51, SDL_ALPHA_OPAQUE);
+                    for (int i = 0; i < vectsize; i++)
+                    {
+                        std::string row = infotemp[i];
+                        std::vector<std::string> entirevector = _helper.split(row, ',');
+                        int x1 = std::stoi(entirevector[0]) / MajorDivider;
+                        int y1 = std::stoi(entirevector[1]) / MajorDivider;
+                        int x2 = std::stoi(entirevector[2]) / MajorDivider;
+                        int y2 = std::stoi(entirevector[3]) / MajorDivider;
+                        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+                        
+                    }
+                    SDL_RenderPresent(renderer);
+                    //SDL_Delay(3000);
+                        //SDL_Delay(3000);
                 }
             }
+            //while (!done) 
+            //{
+            //    SDL_Event event;
+
+            //    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+            //    SDL_RenderClear(renderer);
+
+            //    SDL_SetRenderDrawColor(renderer, 255, 255, 51, SDL_ALPHA_OPAQUE);
+            //    for (int i = 0; i < vectsize; i++)
+            //    {
+            //        std::string row = info[i];
+            //        std::vector<std::string> entirevector = _helper.split(row, ',');
+            //        int x1 = std::stoi(entirevector[0]) / MajorDivider;
+            //        int y1 = std::stoi(entirevector[1]) / MajorDivider;
+            //        int x2 = std::stoi(entirevector[2]) / MajorDivider;
+            //        int y2 = std::stoi(entirevector[3]) / MajorDivider;
+            //        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+            //        SDL_RenderPresent(renderer);
+            //        //SDL_Delay(50);
+            //    }
+            //    
+            //    SDL_Delay(19000);
+            //    break;
+
+            //    while (SDL_PollEvent(&event)) {
+            //        if (event.type == SDL_QUIT) {
+            //            done = SDL_TRUE;
+            //        }
+            //    }
+            //}
         }
 
         if (renderer) {
